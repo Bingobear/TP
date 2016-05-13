4ff37641a73c66533faaefceb113cccd57de40af
SimonBrunsCC <simon.bruns@codecentric.de>
2016-05-13 13:19:38 +0200

    reformatted

--- src/main/java/models/Word.java
+++ src/main/java/models/Word.java
@@ -4,66 +4,65 @@ import java.util.ArrayList;
 
 /**
  * Word model class
- * 
- * @author Simon Bruns
  *
+ * @author Simon Bruns
  */
 
 public class Word {
-	private String text;
-	private String stem;
-	private String type;
-	private ArrayList<Category> category;
+    private String text;
+    private String stem;
+    private String type;
+    private ArrayList<Category> category;
 
-	
-	public Word() {
-	}
-	
-	public Word(String text, String stem, String type, ArrayList<Category> cat) {
-		this.text = text;
-		this.stem = stem;
-		this.type = type;
-		this.category = cat;
-	}
 
-	public Word(Word word) {
-		this.text = word.text;
-		this.stem = word.stem;
-		this.type = word.type;
-		this.category = word.category;
-	}
+    public Word() {
+    }
 
-	public String getText() {
-		return text;
-	}
+    public Word(String text, String stem, String type, ArrayList<Category> cat) {
+        this.text = text;
+        this.stem = stem;
+        this.type = type;
+        this.category = cat;
+    }
 
-	public void setText(String text) {
-		this.text = text;
-	}
+    public Word(Word word) {
+        this.text = word.text;
+        this.stem = word.stem;
+        this.type = word.type;
+        this.category = word.category;
+    }
 
-	public String getStem() {
-		return stem;
-	}
+    public String getText() {
+        return text;
+    }
 
-	public void setStem(String stem) {
-		this.stem = stem;
-	}
+    public void setText(String text) {
+        this.text = text;
+    }
 
-	public String getType() {
-		return type;
-	}
+    public String getStem() {
+        return stem;
+    }
 
-	public void setType(String type) {
-		this.type = type;
-	}
+    public void setStem(String stem) {
+        this.stem = stem;
+    }
 
-	public ArrayList<Category> getCategory() {
-		return category;
-	}
+    public String getType() {
+        return type;
+    }
 
-	public void setCategory(ArrayList<Category> category) {
-		this.category = category;
-	}
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public ArrayList<Category> getCategory() {
+        return category;
+    }
+
+    public void setCategory(ArrayList<Category> category) {
+        this.category = category;
+    }
 
 
 }
